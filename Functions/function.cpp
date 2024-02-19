2cc2640635958ebd7ef35a2bd7fe1f67bbdd099f
Shrinivas Mahendra Kangralkar <88592860+Shri8055@users.noreply.github.com>
2024-02-19 17:05:13 +0530

    Add files via upload

---
+++ Functions/function.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int slog(){
+    cout<<endl<<"Message from Function";
+}
+int main(){
+    int a,b;
+    cout<<"Enter number:  ";
+    cin>>a;
+    cout<<"Number is:   "<<a;
+    slog();
+}
